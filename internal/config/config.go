91cad42f2ee54d554f7c72a679bdaa2abee97575
driftdev <92285161+driftdev@users.noreply.github.com>
2024-07-20 20:19:42 +0530

    fix: updated the configuration to be structured and extensible

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,61 +1,89 @@
 package config
 
 type ServerConfiguration struct {
-	ServerHost        string `mapstructure:"DRIFTHOOK_SERVER_HOST"`
-	ServerPort        uint   `mapstructure:"DRIFTHOOK_SERVER_PORT"`
-	ServerEnvironment string `mapstructure:"DRIFTHOOK_SERVER_ENVIRONMENT"`
+	Host string `json:"host" yaml:"host" mapstructure:"DRIFTHOOK_SERVER_HOST"`
+	Port uint   `json:"port" yaml:"port" mapstructure:"DRIFTHOOK_SERVER_PORT"`
+	SSL  bool   `json:"ssl" yaml:"ssl" mapstructure:"DRIFTHOOK_SERVER_SSL"`
+	Env  string `json:"env" yaml:"env" mapstructure:"DRIFTHOOK_SERVER_ENV"`
+}
+
+type PostgresUserConfiguration struct {
+	Username string `json:"username" yaml:"username" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_USERNAME"`
+	Password string `json:"password" yaml:"password" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_PASSWORD"`
+}
+
+type PostgresConfiguration struct {
+	Host              string                    `json:"host" yaml:"host" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_HOST"`
+	Port              uint                      `json:"port" yaml:"port" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_PORT"`
+	User              PostgresUserConfiguration `json:"user" yaml:"user" mapstructure:",squash"`
+	DatabaseName      string                    `json:"database_name" yaml:"databaseName" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_DATABASE_NAME"`
+	Options           string                    `json:"options" yaml:"options" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_OPTIONS"`
+	MaxOpenConnection uint                      `json:"max_open_connection" yaml:"maxOpenConnection" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_MAX_OPEN_CONNECTION"`
+	MaxIdleConnection uint                      `json:"max_idle_connection" yaml:"maxIdleConnection" mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_MAX_IDLE_CONNECTION"`
 }
 
 type DatabaseConfiguration struct {
-	PostgresHost         string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_HOST"`
-	PostgresPort         uint   `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_PORT"`
-	PostgresUserUsername string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_USERNAME"`
-	PostgresUserPassword string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_PASSWORD"`
-	PostgresDatabaseName string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_DATABASE_NAME"`
-	PostgresOptions      string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_OPTIONS"`
+	Postgres PostgresConfiguration `json:"postgres" yaml:"postgres" mapstructure:",squash"`
+}
+
+type NATSConfiguration struct {
+	Urls     []string `json:"urls" yaml:"urls" mapstructure:"DRIFTHOOK_QUEUE_NATS_URLS"`
+	Username string   `json:"username" yaml:"username" mapstructure:"DRIFTHOOK_QUEUE_NATS_USERNAME"`
+	Password string   `json:"password" yaml:"password" mapstructure:"DRIFTHOOK_QUEUE_NATS_PASSWORD"`
 }
 
 type QueueConfiguration struct {
-	NATSUrl      string `mapstructure:"DRIFTHOOK_QUEUE_NATS_URL"`
-	NATSUsername string `mapstructure:"DRIFTHOOK_QUEUE_NATS_USERNAME"`
-	NATSPassword string `mapstructure:"DRIFTHOOK_QUEUE_NATS_PASSWORD"`
+	NATS NATSConfiguration `json:"nats" yaml:"nats" mapstructure:",squash"`
+}
+
+type S3Configuration struct {
+	AccessKey      string `json:"access_key" yaml:"accessKey" mapstructure:"DRIFTHOOK_STORAGE_S3_ACCESS_KEY"`
+	SecretKey      string `json:"secret_key" yaml:"secretKey" mapstructure:"DRIFTHOOK_STORAGE_S3_SECRET_KEY"`
+	BucketName     string `json:"bucket_name" yaml:"bucketName" mapstructure:"DRIFTHOOK_STORAGE_S3_BUCKET_NAME"`
+	PathPrefix     string `json:"path_prefix" yaml:"pathPrefix" mapstructure:"DRIFTHOOK_STORAGE_S3_PATH_PREFIX"`
+	Region         string `json:"region" yaml:"region" mapstructure:"DRIFTHOOK_STORAGE_S3_REGION"`
+	ForcePathStyle bool   `json:"force_path_style" yaml:"forcePathStyle" mapstructure:"DRIFTHOOK_STORAGE_S3_FORCE_PATH_STYLE"`
+	DisableSSL     bool   `json:"disable_ssl" yaml:"disableSSL" mapstructure:"DRIFTHOOK_STORAGE_S3_DISABLE_SSL"`
 }
 
 type StorageConfiguration struct {
-	S3AccessKey      string `mapstructure:"DRIFTHOOK_STORAGE_S3_ACCESS_KEY"`
-	S3SecretKey      string `mapstructure:"DRIFTHOOK_STORAGE_S3_SECRET_KEY"`
-	S3BucketName     string `mapstructure:"DRIFTHOOK_STORAGE_S3_BUCKET_NAME"`
-	S3PathPrefix     string `mapstructure:"DRIFTHOOK_STORAGE_S3_PATH_PREFIX"`
-	S3Region         string `mapstructure:"DRIFTHOOK_STORAGE_S3_REGION"`
-	S3ForcePathStyle bool   `mapstructure:"DRIFTHOOK_STORAGE_S3_FORCE_PATH_STYLE"`
-	S3DisableSSL     bool   `mapstructure:"DRIFTHOOK_STORAGE_S3_DISABLE_SSL"`
+	S3 S3Configuration `json:"s3" yaml:"s3" mapstructure:",squash"`
 }
 
 type Configuration struct {
-	ServerConfiguration   ServerConfiguration   `mapstructure:",squash"`
-	DatabaseConfiguration DatabaseConfiguration `mapstructure:",squash"`
-	QueueConfiguration    QueueConfiguration    `mapstructure:",squash"`
-	StorageConfiguration  StorageConfiguration  `mapstructure:",squash"`
+	Server   ServerConfiguration   `json:"server" yaml:"server" mapstructure:",squash"`
+	Database DatabaseConfiguration `json:"database" yaml:"database" mapstructure:",squash"`
+	Queue    QueueConfiguration    `json:"queue" yaml:"queue" mapstructure:",squash"`
+	Storage  StorageConfiguration  `json:"storage" yaml:"storage" mapstructure:",squash"`
 }
 
 var DefaultConfiguration = Configuration{
-	ServerConfiguration: ServerConfiguration{
-		ServerHost: "localhost",
-		ServerPort: 80,
+	Server: ServerConfiguration{
+		Host: "localhost",
+		Port: 8100,
+		Env:  "dev",
 	},
 
-	DatabaseConfiguration: DatabaseConfiguration{
-		PostgresHost:         "localhost",
-		PostgresPort:         5432,
-		PostgresUserUsername: "postgres",
-		PostgresUserPassword: "postgres",
-		PostgresDatabaseName: "drifthook",
-		PostgresOptions:      "",
+	Database: DatabaseConfiguration{
+		Postgres: PostgresConfiguration{
+			Host: "localhost",
+			Port: 5432,
+			User: PostgresUserConfiguration{
+				Username: "postgres",
+				Password: "postgres",
+			},
+			DatabaseName:      "drifthook",
+			Options:           "sslmode=disable",
+			MaxOpenConnection: 15,
+			MaxIdleConnection: 12,
+		},
 	},
 
-	QueueConfiguration: QueueConfiguration{
-		NATSUrl:      "nats://localhost:4222",
-		NATSUsername: "nats",
-		NATSPassword: "nats",
+	Queue: QueueConfiguration{
+		NATS: NATSConfiguration{
+			Urls:     []string{"nats://localhost:4222"},
+			Username: "nats",
+			Password: "nats",
+		},
 	},
 }
