2c726e5692bb9f56d834b051184358680239abc6
driftdev <92285161+driftdev@users.noreply.github.com>
2024-07-20 12:25:43 +0530

    fix: updated the configuration to use specific config names explicitly

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,33 +1,61 @@
 package config
 
+type ServerConfiguration struct {
+	ServerHost        string `mapstructure:"DRIFTHOOK_SERVER_HOST"`
+	ServerPort        uint   `mapstructure:"DRIFTHOOK_SERVER_PORT"`
+	ServerEnvironment string `mapstructure:"DRIFTHOOK_SERVER_ENVIRONMENT"`
+}
+
+type DatabaseConfiguration struct {
+	PostgresHost         string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_HOST"`
+	PostgresPort         uint   `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_PORT"`
+	PostgresUserUsername string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_USERNAME"`
+	PostgresUserPassword string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_USER_PASSWORD"`
+	PostgresDatabaseName string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_DATABASE_NAME"`
+	PostgresOptions      string `mapstructure:"DRIFTHOOK_DATABASE_POSTGRES_OPTIONS"`
+}
+
+type QueueConfiguration struct {
+	NATSUrl      string `mapstructure:"DRIFTHOOK_QUEUE_NATS_URL"`
+	NATSUsername string `mapstructure:"DRIFTHOOK_QUEUE_NATS_USERNAME"`
+	NATSPassword string `mapstructure:"DRIFTHOOK_QUEUE_NATS_PASSWORD"`
+}
+
+type StorageConfiguration struct {
+	S3AccessKey      string `mapstructure:"DRIFTHOOK_STORAGE_S3_ACCESS_KEY"`
+	S3SecretKey      string `mapstructure:"DRIFTHOOK_STORAGE_S3_SECRET_KEY"`
+	S3BucketName     string `mapstructure:"DRIFTHOOK_STORAGE_S3_BUCKET_NAME"`
+	S3PathPrefix     string `mapstructure:"DRIFTHOOK_STORAGE_S3_PATH_PREFIX"`
+	S3Region         string `mapstructure:"DRIFTHOOK_STORAGE_S3_REGION"`
+	S3ForcePathStyle bool   `mapstructure:"DRIFTHOOK_STORAGE_S3_FORCE_PATH_STYLE"`
+	S3DisableSSL     bool   `mapstructure:"DRIFTHOOK_STORAGE_S3_DISABLE_SSL"`
+}
+
 type Configuration struct {
-	ServerHost string `mapstructure:"DRIFTHOOK_SERVER_HOST"`
-	ServerPort uint   `mapstructure:"DRIFTHOOK_SERVER_PORT"`
-
-	PostgresHost         string `mapstructure:"DRIFTHOOK_POSTGRES_HOST"`
-	PostgresPort         uint   `mapstructure:"DRIFTHOOK_POSTGRES_PORT"`
-	PostgresUserUsername string `mapstructure:"DRIFTHOOK_POSTGRES_USER_USERNAME"`
-	PostgresUserPassword string `mapstructure:"DRIFTHOOK_POSTGRES_USER_PASSWORD"`
-	PostgresDatabaseName string `mapstructure:"DRIFTHOOK_POSTGRES_DATABASE_NAME"`
-	PostgresOptions      string `mapstructure:"DRIFTHOOK_POSTGRES_OPTIONS"`
-
-	NATSUrl      string `mapstructure:"DRIFTHOOK_NATS_URL"`
-	NATSUsername string `mapstructure:"DRIFTHOOK_NATS_USERNAME"`
-	NATSPassword string `mapstructure:"DRIFTHOOK_NATS_PASSWORD"`
+	ServerConfiguration   ServerConfiguration   `mapstructure:",squash"`
+	DatabaseConfiguration DatabaseConfiguration `mapstructure:",squash"`
+	QueueConfiguration    QueueConfiguration    `mapstructure:",squash"`
+	StorageConfiguration  StorageConfiguration  `mapstructure:",squash"`
 }
 
 var DefaultConfiguration = Configuration{
-	ServerHost: "localhost",
-	ServerPort: 8080,
+	ServerConfiguration: ServerConfiguration{
+		ServerHost: "localhost",
+		ServerPort: 80,
+	},
 
-	PostgresHost:         "localhost",
-	PostgresPort:         5432,
-	PostgresUserUsername: "postgres",
-	PostgresUserPassword: "postgres",
-	PostgresDatabaseName: "drifthook",
-	PostgresOptions:      "",
+	DatabaseConfiguration: DatabaseConfiguration{
+		PostgresHost:         "localhost",
+		PostgresPort:         5432,
+		PostgresUserUsername: "postgres",
+		PostgresUserPassword: "postgres",
+		PostgresDatabaseName: "drifthook",
+		PostgresOptions:      "",
+	},
 
-	NATSUrl:      "nats://localhost:4222",
-	NATSUsername: "",
-	NATSPassword: "",
+	QueueConfiguration: QueueConfiguration{
+		NATSUrl:      "nats://localhost:4222",
+		NATSUsername: "nats",
+		NATSPassword: "nats",
+	},
 }
