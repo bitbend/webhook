2e0b7b554d2f999adf6c572b71db1ec57fdf523e
driftdev <92285161+driftdev@users.noreply.github.com>
2024-08-03 20:35:43 +0530

    feat: added event group field

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -11,10 +11,17 @@ func (ei EventId) String() string {
 	return string(ei)
 }
 
+type EventType string
+
+func (et EventType) String() string {
+	return string(et)
+}
+
 type Event struct {
 	Id        EventId
 	TenantId  TenantId
 	StreamId  StreamId
+	GroupId   *EndpointGroupId
 	Data      json.RawMessage
 	CreatedAt time.Time
 	UpdatedAt time.Time
