4bce58a2d6af088ea16e6b2e10d4e5a3d0e7ef77
driftdev <92285161+driftdev@users.noreply.github.com>
2024-08-03 20:37:05 +0530

    feat: added project configuration and project status

--- internal/domain/project.go
+++ internal/domain/project.go
@@ -8,13 +8,32 @@ func (pi ProjectId) String() string {
 	return string(pi)
 }
 
+type ProjectStatus string
+
+const (
+	ProjectStatusActive   ProjectStatus = "active"
+	ProjectStatusInactive ProjectStatus = "inactive"
+)
+
+type ProjectConfiguration struct {
+	DisableEndpoints   bool
+	RetentionPolicy    string
+	AllowedPayloadSize int
+	RateLimitCount     int
+	RateLimitDuration  time.Duration
+	RetryStrategy      string
+	RetryCount         int
+	RetryDuration      time.Duration
+}
+
 type Project struct {
 	Id             ProjectId
 	TenantId       TenantId
 	OrganizationId OrganizationId
 	Name           string
 	Description    *string
-	ApiKey         string
+	Configuration  *ProjectConfiguration
+	Status         ProjectStatus
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 }
