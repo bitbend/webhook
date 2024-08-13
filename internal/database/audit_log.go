b8676095d02749be1896d72789eaa2beccad59bb
driftdev <92285161+driftdev@users.noreply.github.com>
2024-08-13 18:36:55 +0530

    refactor: changes nullable fields into pointer values and replaced json.RawMessage with []byte

--- internal/database/audit_log.go
+++ internal/database/audit_log.go
@@ -1,17 +1,15 @@
 package database
 
 import (
-	"database/sql"
-	"encoding/json"
 	"github.com/uptrace/bun"
 	"time"
 )
 
 type AuditLog struct {
 	bun.BaseModel `bun:"table:audit_logs"`
-	Id            string          `bun:"id,pk"`
-	TenantId      sql.NullString  `bun:"tenant_id"`
-	Name          string          `bun:"name"`
-	Data          json.RawMessage `bun:"data"`
-	CreatedAt     time.Time       `bun:"created_at"`
+	Id            string    `bun:"id,pk"`
+	TenantId      *string   `bun:"tenant_id"`
+	Name          string    `bun:"name"`
+	Data          []byte    `bun:"data"`
+	CreatedAt     time.Time `bun:"created_at"`
 }
--- internal/database/deleted_record.go
+++ internal/database/deleted_record.go
@@ -1,18 +1,16 @@
 package database
 
 import (
-	"database/sql"
-	"encoding/json"
 	"github.com/uptrace/bun"
 	"time"
 )
 
 type DeletedRecord struct {
 	bun.BaseModel `bun:"table:deleted_records"`
-	Id            string          `bun:"id,pk"`
-	TenantId      sql.NullString  `bun:"tenant_id"`
-	TableName     string          `bun:"table_name"`
-	RecordId      string          `bun:"record_id"`
-	Data          json.RawMessage `bun:"data"`
-	DeletedAt     time.Time       `bun:"deleted_at"`
+	Id            string    `bun:"id,pk"`
+	TenantId      *string   `bun:"tenant_id"`
+	TableName     string    `bun:"table_name"`
+	RecordId      string    `bun:"record_id"`
+	Data          []byte    `bun:"data"`
+	DeletedAt     time.Time `bun:"deleted_at"`
 }
